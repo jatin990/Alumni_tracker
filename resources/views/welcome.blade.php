bdd5abf7654028ebc5fcb54efd52ca83705728e6
jatin <jatinabrol288@gmail.com>
2020-01-19 17:00:16 +0530

    m

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,14 +85,14 @@
                 </div>
 
                 <div class="links">
-                <a href="{{ route('d_admin.dashboard')}}">Docs</a>
+                <a href="{{ route('d_admin.login')}}">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href=" {{ route('c_admin.dashboard')}}">GitHub</a>
+                    <a href=" {{ route('c_admin.login')}}">GitHub</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -31,14 +31,14 @@ Route::prefix('c_admin')->group(function () {
     Route::get('/login', 'Auth\C_adminLoginController@showLoginForm')->name('c_admin.login');
     Route::post('/login', 'Auth\C_adminLoginController@login')->name('c_admin.login.submit');
     Route::get('/', 'C_adminController@index')->name('c_admin.dashboard');
-    Route::get('/logout', 'Auth\C_adminLoginController@logout')->name('admin.logout');
+    Route::get('/logout', 'Auth\C_adminLoginController@logout')->name('c_admin.logout');
 });
 
 
 
 Route::prefix('d_admin')->group(function () {
-    Route::get('/login', 'Auth\D_adminLoginController@showLoginForm')->name('admin.login');
+    Route::get('/login', 'Auth\D_adminLoginController@showLoginForm')->name('d_admin.login');
     Route::post('/login', 'Auth\D_adminLoginController@login')->name('d_admin.login.submit');
     Route::get('/', 'D_adminController@index')->name('d_admin.dashboard');
-    Route::get('/logout', 'Auth\D_adminLoginController@logout')->name('admin.logout');
+    Route::get('/logout', 'Auth\D_adminLoginController@logout')->name('d_admin.logout');
 });
